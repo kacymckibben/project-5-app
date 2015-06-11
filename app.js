3fd6afdc8f2f894f44a3f6b421505c80db07486a
Kacy McKibben <kacy.mckibben@gmail.com>
2015-06-11 13:18:22 -0700

    updated Model and VM

--- app.js
+++ app.js
@@ -1,52 +1,97 @@
-function Model() {
-	this.generateLists = function(articles) {
-		var Info_Content = '<ul style="list-style-type:none">\n';
-
-		for (var i = 0; i < articles.length; i++){
-			var name = articles[i].name;
-			var url = articles[i].url;
-			var rating = articles[i].rating;
-			var checkinCount = articles[i].checkinCount;
-			var street = articles[i].street;
-			var cityState = articles[i].cityState;
-
-			Info_Content += '<li><a href="' + url + '">' + name + '</a></li>\n<li>FourSquare Rating: ' + rating + '</li>\n<li>Check Ins: ' + checkinCount + '</li>\n<li>' + street + '</li>\n<li>' + cityState + '</li>';
-		}
-		Info_Content += '<ul>';
-		return Info_Content;
-	};
+var initialPlaces = [
+	{
+		name: "The LA Hotel Downtown",
+		lat: 34.05497,
+		lon: -118.255428
+	},
+	{
+		name: "Yard House",
+		lat: 34.044984,
+		lon: -118.265775
+	},
+	{
+		name: "Walt Disney Concert Hall",
+		lat: 34.055345,
+		lon: -118.249845
+	},
+	{
+		name: "Dodger Stadium",
+		lat: 34.072736,
+		lon: -118.240616
+	},
+	{
+		name: "Metro Plaza Hotel",
+		lat: 34.058834,
+		lon: -118.237616
+	},
+	{
+		name: "Historic Mayfair Hotel",
+		lat: 34.052108,
+		lon: -118.26764
+	},
+	{
+		name: "Bottega Louie",
+		lat: 34.0475211,
+		lon: -118.2640137
+	},
+	{
+		name: "The Original Pantry Cafe",
+		lat: 34.0464657,
+		lon: -118.262997
+	},
+	{
+		name: "Hotel Figueroa",
+		lat: 34.0464665,
+		lon: -118.2629968
+	},
+	{
+		name: "Natural History Museum of Los Angeles",
+		lat: 34.017089,
+		lon: -118.28876
+	}
+];
+var Place = function(data){
+	this.name = ko.observable(data.name);
+	this.lat = ko.observable(data.lat);
+	this.lon = ko.observable(data.lon);
+	this.marker = ko.observable();
+	this.rating = ko.observable();
+	this.checkinCount = ko.observable();
+	this.street = ko.observable();
+	this.cityState = ko.observable();
+	this.url = ko.observable();
 }
-var model = new Model();
-
 var ViewModel = function() {
 	var self = this;
-	self.articleList = ko.observableArray();
-
-	self.article = function (name, url, rating, checkinCount, street, cityState) {
-		this.name = name;
-		this.url = url;
-		this.rating = rating;
-		this.checkinCount = checkinCount;
-		this.street = street;
-		this.cityState = cityState;
-	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-		center: new google.maps.LatLng(34.0432121, -118.2499534),
-		zoom: 12,
-	});
+	this.placeList = ko.observableArray([]);
 
 	var infowindow = new google.maps.InfoWindow();
-
-	self.mapPin = function(name, lat, lon) {
-		this.name = ko.observable(name);
-		this.lat = ko.observable(lat);
-		this.lon = ko.observable(lon);
-
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(lat, lon),
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+			center: new google.maps.LatLng(34.0432121, -118.2499534),
+			zoom: 12,
+	});	
+	var marker;
+	for (var i = 0; i < initialPlaces.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(initialPlaces[i].lat, initialPlaces[i].lon),
 			map: map,
 			animation: google.maps.Animation.DROP,
 		});
+		var foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?limit=1&ll=' + initialPlaces[i].lat + ',' + initialPlaces[i].lon + '&intent=match&query=' + initialPlaces[i].name + '&client_id=AOICOBBHGVYWPCIOEKTNF5CECB3RNMJWJWIQHEHJ1ZWDSAH1&client_secret=5HO3PIPDPY1DS50SLOUFNIAU1ZO2YBPPSWJQDCFGCBKE3HND&v=20140806';
+		var failText = 'Failed to get FOURSQUARE resources';
+		var name, url, rating, checkinCount, street, cityState;
+		$.getJSON(foursquareUrl, function(data){
+			results = data.response.groups[0].items[0].venue;
+			name = results.name;
+			url = results.url;
+			rating = results.rating;
+			checkinCount = results.stats.checkinsCount;
+			street = results.location.formattedAddress[0];
+			cityState = results.location.formattedAddress[1];
+
+			this.placeList = new Place(name, marker, url, rating, checkinCount, street, cityState);
+		console.log(this.placeList.street);
+	});
 
 		function toggleBounce() {
 			if(marker.getAnimation() != null) {
@@ -57,50 +102,17 @@ var ViewModel = function() {
 		}
 
 		google.maps.event.addListener(marker, 'click', function(){
-			self.apiData(name,lat,lon);
-
 			toggleBounce();
 			setTimeout(toggleBounce, 2000);
 			setTimeout(function(){
-				infowindow.setContent(model.generateLists(self.articleList()));
+				infowindow.setContent('<p>test' + this.placeList[i].street + '</p>');
 				infowindow.open(map, marker);
 			}, 1000);
 		});
-	};
-
-	self.pins = ko.observableArray([
-		new self.mapPin("The LA Hotel Downtown", 34.05497, -118.255428),
-		new self.mapPin("Yard House", 34.044984, -118.265775),
-		new self.mapPin("Walt Disney Concert Hall", 34.055345, -118.249845),
-		new self.mapPin("Dodger Stadium", 34.072736, -118.240616),
-		new self.mapPin("Metro Plaza Hotel",34.058834,-118.237616),
-		new self.mapPin("Historic Mayfair Hotel",34.052108,-118.26764),
-		new self.mapPin("Wokcano",34.0511124,-118.2669319),
-		new self.mapPin("Bottega Louie",34.0475211,-118.2640137),
-		new self.mapPin("The Original Pantry Cafe",34.0464657,-118.262997),
-		new self.mapPin("Hotel Figueroa",34.0464665,-118.2629968),
-		new self.mapPin("Natural History Museum of Los Angeles",34.017089,-118.28876)
-	]);
-
-	self.apiData = function(name,lat,lon) {
-		var foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?limit=1&ll=' + lat + ',' + lon + '&intent=match&query=' + name + '&client_id=AOICOBBHGVYWPCIOEKTNF5CECB3RNMJWJWIQHEHJ1ZWDSAH1&client_secret=5HO3PIPDPY1DS50SLOUFNIAU1ZO2YBPPSWJQDCFGCBKE3HND&v=20140806';
-		var failText = 'Failed to get FOURSQUARE resources';
-
-		$.getJSON(foursquareUrl, function(data){
-			articles = data.response.groups[0].items[0].venue;
-			self.articleList.removeAll();
-			var name = articles.name;
-			var url = articles.url;
-			var rating = articles.rating;
-			var checkinCount = articles.stats.checkinsCount;
-			var street = articles.location.formattedAddress[0];
-			var cityState = articles.location.formattedAddress[1];
-			self.articleList.push(new self.article(name, url, rating, checkinCount, street, cityState));
-		});
 	}
 
-	self.show_info = function(pins){
-		google.maps.event.trigger(pins.marker,'click');
+	this.show_info = function(Place){
+		google.maps.event.trigger(Place.marker,'click');
 	}
 };
 $(document).ready(function(){
