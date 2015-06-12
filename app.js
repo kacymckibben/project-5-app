5e481a7c7206666446e0d2f1ecd6c89aabfa49fd
Kacy McKibben <kacy.mckibben@gmail.com>
2015-06-11 21:40:45 -0700

    info window and binding to show_info

--- app.js
+++ app.js
@@ -64,58 +64,77 @@ var Place = function(data){
 var ViewModel = function() {
 	var self = this;
 	this.placeList = ko.observableArray([]);
+	/*this.filter = ko.observable("");*/
+
 	initialPlaces.forEach(function(placeItem){
 		self.placeList.push( new Place(placeItem) );
 	});
+
 	var infowindow = new google.maps.InfoWindow();
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
 			center: new google.maps.LatLng(34.0432121, -118.2499534),
 			zoom: 12,
 	});	
+
 	var marker;
-	for (var i = 0; i < this.placeList().length; i++) {
+	this.placeList().forEach(function(placeItem){
 		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(this.placeList()[i].lat(), this.placeList()[i].lon()),
+			position: new google.maps.LatLng(placeItem.lat(),placeItem.lon()),
 			map: map,
 			animation: google.maps.Animation.DROP,
 		});
-		var foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?limit=1&ll=' + initialPlaces[i].lat + ',' + initialPlaces[i].lon + '&intent=match&query=' + initialPlaces[i].name + '&client_id=AOICOBBHGVYWPCIOEKTNF5CECB3RNMJWJWIQHEHJ1ZWDSAH1&client_secret=5HO3PIPDPY1DS50SLOUFNIAU1ZO2YBPPSWJQDCFGCBKE3HND&v=20140806';
+		placeItem.marker = marker;
+
+		var foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?limit=1&ll=' + placeItem.lat() + ',' + placeItem.lon() + '&intent=match&query=' + placeItem.name() + '&client_id=AOICOBBHGVYWPCIOEKTNF5CECB3RNMJWJWIQHEHJ1ZWDSAH1&client_secret=5HO3PIPDPY1DS50SLOUFNIAU1ZO2YBPPSWJQDCFGCBKE3HND&v=20140806';
 		var failText = 'Failed to get FOURSQUARE resources';
 		var name, url, rating, checkinCount, street, cityState;
 		$.getJSON(foursquareUrl, function(data){
 			results = data.response.groups[0].items[0].venue;
-			name = results.name;
-			url = results.url;
-			rating = results.rating;
-			checkinCount = results.stats.checkinsCount;
-			street = results.location.formattedAddress[0];
-			cityState = results.location.formattedAddress[1];
-			self.placeList()[i] = new Place(name, url, rating, checkinCount, street, cityState);
-			console.log(self.placeList()[i].street);
+			placeItem.name = results.name;
+			placeItem.url = results.url;
+			placeItem.rating = results.rating;
+			placeItem.checkinCount = results.stats.checkinsCount;
+			placeItem.street = results.location.formattedAddress[0];
+			placeItem.cityState = results.location.formattedAddress[1];
 		});
 
 		function toggleBounce() {
-			if(marker.getAnimation() != null) {
-				marker.setAnimation(null);
+			if(placeItem.marker.getAnimation() != null) {
+				placeItem.marker.setAnimation(null);
 			} else {
-				marker.setAnimation(google.maps.Animation.BOUNCE);
+				placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
 			}
 		}
 
-		google.maps.event.addListener(marker, 'click', function(){
+		google.maps.event.addListener(placeItem.marker, 'click', function(){
 			toggleBounce();
 			setTimeout(toggleBounce, 2000);
 			setTimeout(function(){
-				infowindow.setContent('<p>test' + self.placeList()[i].street + '</p>');
-				infowindow.open(map, marker);
+				infowindow.setContent('<h2>' + placeItem.name + '</h2>\n<p>Rating: ' + placeItem.rating + '</p>\n<p>Check Ins: ' + placeItem.checkinCount + '</p>\n<a href=' + placeItem.url + '>' + placeItem.url + '</a>\n<p>Address:</p>\n<p>' + placeItem.street + '</p>\n<p>' + placeItem.cityState + '</p>');
+				infowindow.open(map, placeItem.marker);
 			}, 1000);
 		});
-	}
 
-	this.show_info = function(){
-		google.maps.event.trigger(marker,'click');
-	}
-};
+	});
+
+	self.show_info = function(placeItem){
+		google.maps.event.trigger(placeItem.marker,'click');
+	};
+	
+	this.filter = ko.observable("");
+	this.filteredItems = ko.computed(function() {
+		var filter = self.filter().toLowerCase();
+		if(!filter) {
+			return self.placeList();
+		} else {
+			return ko.utils.arrayFilter(self.placeList(), function(placeItem) {
+				return ko.utils.stringStartsWith(placeItem.name().toLowerCase(), filter);
+			});
+		}
+	}, this);
+}
+
 $(document).ready(function(){
 	ko.applyBindings(new ViewModel());
 });
+
