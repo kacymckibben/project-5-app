710dda49a5c0d463b79a7316c26b1715ebc2206f
Kacy McKibben <kacy.mckibben@gmail.com>
2015-06-12 23:25:43 -0700

    finished functionality, working on layout and extras

--- app.js
+++ app.js
@@ -66,25 +66,31 @@ var ViewModel = function() {
 	this.placeList = ko.observableArray([]);
 	/*this.filter = ko.observable("");*/
 
+	/*Creates new Place objects for each item in initialPlaces*/
 	initialPlaces.forEach(function(placeItem){
 		self.placeList.push( new Place(placeItem) );
 	});
 
+	/*Creates a new infowindow object*/
 	var infowindow = new google.maps.InfoWindow();
+	/*Creates the map*/
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
 			center: new google.maps.LatLng(34.0432121, -118.2499534),
 			zoom: 12,
 	});	
 
+	/*For each placeItem, create a marker, look up FourSquare info, and add Listener to each marker*/
 	var marker;
 	this.placeList().forEach(function(placeItem){
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(placeItem.lat(),placeItem.lon()),
 			map: map,
 			animation: google.maps.Animation.DROP,
+			title: placeItem.name()
 		});
 		placeItem.marker = marker;
 
+		/*Look up FourSquare info*/
 		var foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?limit=1&ll=' + placeItem.lat() + ',' + placeItem.lon() + '&intent=match&query=' + placeItem.name() + '&client_id=AOICOBBHGVYWPCIOEKTNF5CECB3RNMJWJWIQHEHJ1ZWDSAH1&client_secret=5HO3PIPDPY1DS50SLOUFNIAU1ZO2YBPPSWJQDCFGCBKE3HND&v=20140806';
 		var failText = 'Failed to get FOURSQUARE resources';
 		var name, url, rating, checkinCount, street, cityState;
@@ -98,6 +104,7 @@ var ViewModel = function() {
 			placeItem.cityState = results.location.formattedAddress[1];
 		});
 
+		/*Toggles the bounce animation on the marker*/
 		function toggleBounce() {
 			if(placeItem.marker.getAnimation() != null) {
 				placeItem.marker.setAnimation(null);
@@ -106,22 +113,57 @@ var ViewModel = function() {
 			}
 		}
 
+		/*When the marker is clicked, animate the marker and show infowindow*/
 		google.maps.event.addListener(placeItem.marker, 'click', function(){
 			toggleBounce();
-			setTimeout(toggleBounce, 2000);
+			setTimeout(toggleBounce, 1500);
 			setTimeout(function(){
-				infowindow.setContent('<h2>' + placeItem.name + '</h2>\n<p>Rating: ' + placeItem.rating + '</p>\n<p>Check Ins: ' + placeItem.checkinCount + '</p>\n<a href=' + placeItem.url + '>' + placeItem.url + '</a>\n<p>Address:</p>\n<p>' + placeItem.street + '</p>\n<p>' + placeItem.cityState + '</p>');
+				infowindow.setContent('<h3>' + placeItem.name + '</h3>\n<p><b>Rating: </b>' + placeItem.rating + '</p>\n<p><b>Check Ins: </b>' + placeItem.checkinCount + '</p>\n<a href=' + placeItem.url + '>' + placeItem.url + '</a>\n<p><b>Address:</b></p>\n<p>' + placeItem.street + '</p>\n<p>' + placeItem.cityState + '</p>');
 				infowindow.open(map, placeItem.marker);
 			}, 1000);
 		});
 
 	});
-
+	/*When list item is clicked, the corresponding marker is clicked and the infowindow is shown*/
 	self.show_info = function(placeItem){
 		google.maps.event.trigger(placeItem.marker,'click');
 	};
-	
-	this.filter = ko.observable("");
+	/*Filter list locations and map markers*/
+
+
+	/*self.currentLocations = ko.computed(function() {
+		return ko.utils.arrayFilter(self.placeList(), function(placeItem) {
+			if (placeItem.name.toLowerCase().match(self.filterTerm().toLowerCase())) {
+				return placeItem;
+			}
+		});
+	});
+	self.currentLocations.subscribe(function() {
+		if (infowindow) {
+			infowindow.close();
+		}
+		for(var i = 0; i < self.placeList().length; i++) {
+			var marker = self.placeList()[i].marker;
+			marker.setMap(null);
+		}
+		setMapMarkers();
+	})*/
+	self.filter = function() {
+		var s = $('#filterTerm').val();
+		console.log(s.toLowerCase().replace(/\b[a-z]/g,"KC"));
+		s = s.toLowerCase().replace(/\b[a-z]/g, function(self) {
+			console.log(self.toUpperCase());
+			return self.toUpperCase();
+		}), $(".locList > li").each(function() {
+			console.log(this);	
+			$(this).text().search(s) > -1 ? $(this).show() : $(this).hide()
+		});
+		for(var i = 0; i < self.placeList().length; i++) {
+			console.log(self.map);
+			self.placeList()[i].marker.setMap(self.placeList()[i].marker.title.search(s) > -1 ? map : null)
+		};
+	}
+	/*this.filter = ko.observable("");
 	this.filteredItems = ko.computed(function() {
 		var filter = self.filter().toLowerCase();
 		if(!filter) {
@@ -131,7 +173,7 @@ var ViewModel = function() {
 				return ko.utils.stringStartsWith(placeItem.name().toLowerCase(), filter);
 			});
 		}
-	}, this);
+	}, this);*/
 }
 
 $(document).ready(function(){
